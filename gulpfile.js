86792a410a5769f23ac0f3e36dc2baf22a406823
srdokinch <srdokinch@gmail.com>
2024-08-03 20:36:44 +0900

    gulpの調整等

--- gulpfile.js
+++ gulpfile.js
@@ -13,8 +13,9 @@ const notify = require('gulp-notify');
 const plumber = require('gulp-plumber');
 const postcss = require("gulp-postcss");
 const mqpacker = require('css-mqpacker');
+const copy = require('gulp-copy');
 
-// プラグインの処理をまとめる
+// scssの処理
 const cssSass = () => {
   return src('src/scss/*.scss') //コンパイル元
   .pipe(plumber({ errorHandler: notify.onError({ message: "<%= error.message %>" })}))
@@ -23,9 +24,23 @@ const cssSass = () => {
   .pipe(dest('dist/css'));   //コンパイル先
 }
 
+// 画像コピータスク
+function copyImages() {
+  return src('src/img/**/*', {encoding: false})
+    .pipe(dest('dist/img'));
+}
+
+// jsをコピー
+function copyJavascript() {
+  return src('src/js/**/*')
+    .pipe(dest('dist/js'));
+}
+
 // タスクをまとめて実行
-exports.default = series(cssSass);
+exports.default = series(cssSass, copyImages, copyJavascript);
 
 exports.watch = function() {
-  watch('src/scss/*.scss', cssSass);
-}
+  watch('src/scss/*.scss', series(cssSass));
+  watch(['src/img/**/*'], series(copyImages));
+  watch(['src/js/**/*'], series(copyJavascript));
+};
