c3b0923d5e1cba06d0d753a9071eecee65224a91
srdokinch <srdokinch@gmail.com>
2024-11-04 21:04:47 +0900

    css書いていく

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,17 +5,28 @@ $(function(){
    const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
-
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
     },
-
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
-
   });
+
+  $(function () {
+    $(window).on("scroll", function () {
+      // ファーストビューの高さを取得
+      if ($(window).scrollTop() > 60) {
+        // スクロールの位置がファーストビューより下の場合にclassを付与
+        $(".header").addClass("js-header");
+      } else {
+        // スクロールの位置がファーストビューより上の場合にclassを外す
+        $(".header").removeClass("js-header");
+      }
+    });
+  });
+
 });
